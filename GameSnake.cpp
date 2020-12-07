717a25b4e7e0f639af138960d9cb73209c864071
Waysyy <74637067+Waysyy@users.noreply.github.com>
2020-12-07 15:01:59 +0700

    Add files via upload

---
+++ GameSnake.cpp
@@ -0,0 +1,58 @@
+﻿// GameSnake
+
+#include <iostream>
+
+using namespace std;
+
+bool GameOver; // изначально false
+
+const int shirina = 50;
+const int visota = 50;
+
+int score, x, y, fruitX, fruitY;
+
+//движения
+
+enum dvij { stop = 0, left, right, up, down};
+
+// Настройка параметров после запуска игры
+
+void Setup() {
+
+    GameOver = false; // пока игрок не проиграл
+
+}
+
+// Прорисовка карты
+
+void Draw() {
+
+}
+
+// Действия пользователя
+
+void vvod() {
+
+}
+
+// Логика игры
+
+void logic() {
+
+}
+
+int main()
+{
+
+    Setup(); // вызов для настройки игры при включении
+
+    while (!GameOver = false) { // пока игра не завершена
+    
+        Draw();
+        vvod();
+        logic();
+
+    }
+    return 0;
+}
+
