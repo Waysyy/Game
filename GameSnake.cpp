62112da43d0ca417e00dfc4bfe6c0b5631034ae2
Waysyy <74637067+Waysyy@users.noreply.github.com>
2020-12-07 15:43:13 +0700

    Add files via upload

--- GameSnake.cpp
+++ GameSnake.cpp
@@ -9,17 +9,28 @@ bool GameOver; // изначально false
 const int shirina = 50;
 const int visota = 50;
 
-int score, x, y, fruitX, fruitY;
+int score, x, y, fruitX, fruitY; //x и y - координаты
 
 //движения
 
 enum dvij { stop = 0, left, right, up, down};
+dvij dv; //управление движениями
 
-// Настройка параметров после запуска игры
+// Изначальные установки
 
 void Setup() {
 
     GameOver = false; // пока игрок не проиграл
+
+      dv = stop; //изначально стоит на месте
+
+          x = shirina / 2;    // изначальные координаты
+          y = visota / 2;
+
+             fruitX = rand() % shirina; // рандомное положение фрукта  
+             fruitY = rand() % visota;
+
+             score = 0;
 
 }
 
